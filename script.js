c190b86c6e0728b374a316481efa6bb4266620ca
Pedrodouglas83 <pedrodouglasrondao@gmail.com>
2025-09-03 20:47:48 -0300

    subindo o arquivo para o git

---
+++ script.js
@@ -0,0 +1,33 @@
+let count =1; 
+document.getElementById("radio1").checked = true; 
+    
+setInterval(function() {
+ nextImage(); 
+}, 2000)
+
+function nextImage(){
+    count++; 
+    if(count>4){
+      count = 1;  
+    } 
+
+document.getElementById("radio"+count).checked = true;     
+
+}
+
+let btnMenu = document.getElementById('btn-menu') 
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click', ()=> {
+  menu.classList.add('abrir-menu')
+})
+
+
+menu.addEventListener('click', ()=> {
+  menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', ()=> {
+  menu.classList.remove('abrir-menu')
+})
